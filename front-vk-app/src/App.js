93f9e8ab4a6f79cd3904b9c33dc4159ddae65a20
ShulgaEvg <yashuler@gmail.com>
2019-05-06 16:19:41 +0700

    test2

---
+++ front-vk-app/src/App.js
@@ -0,0 +1,197 @@
+import React from 'react';
+import connect from '@vkontakte/vkui-connect';
+// import '@vkontakte/vkui/dist/vkui.css';
+import { Route } from "react-router-dom";
+import './App.css';
+import './components/css/Login.css';
+import './components/css/Main.css';
+import Main from './components/js/Main';
+import Railways from './components/js/Railways';
+import Avia from './components/js/Avia';
+import Hotel from './components/js/Hotel';
+import Login from './components/js/Login';
+
+var TERM_URL;
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			userConnect: false,
+			fetchedUser: null,
+			statusUser: false,
+			gadgetPath: null,
+			gadgetId: null,
+			TERM_URL: null,
+		};
+	}
+
+	changeStatusUser = (val) => {
+		this.setState({ statusUser: val });
+	}
+
+	getQueryVariable = (variable) => {
+		var query = window.location.search.substring(1);
+		var vars = query.split("&");
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split("=");
+			if (pair[0] == variable) { return pair[1]; }
+		}
+		return (false);
+	}
+
+	loadWL = (config) => {
+		const hash = Date.now();
+		const container = document.getElementById("whitelabelOTT");
+		const scrollBlock = document.getElementById('main-content');
+		container.innerHTML = "";
+		const gadgetConfig = config || {};
+		gadgetConfig.nodeToScroll = scrollBlock;
+		var isTest = this.getQueryVariable("isTest");
+		console.log("isTest = ", isTest);
+		console.log("get = ", window.location);
+		if (isTest) {
+			gadgetConfig.test = {
+				testPayment: true,
+			};
+		}
+		window[`config_${this.state.gadgetId}`] = gadgetConfig;
+		const script = document.createElement('script');
+		script.src = `${this.state.gadgetPath}${this.state.gadgetId}.js?_=${hash}&ENVID=sandbox-18`;
+		script.async = true;
+		container.appendChild(script);
+		console.log(window[`config_${this.state.gadgetId}`]);
+	}
+
+	startApp = (gadgetPath, gadgetId, config) => {
+		this.setState({
+			gadgetPath: gadgetPath,
+			gadgetId: gadgetId,
+		})
+		setTimeout(() => {
+			window[`gadget_ready_${this.state.gadgetId}`] = () => {
+				window[`gadget_${this.state.gadgetId}`].facade.on('vkpay', (payload) => {
+					console.log("qwe2")
+					if (!payload.TermUrl) {
+						console.error('Отсутствует TermUrl');
+						this.loadWL();
+						return;
+					}
+					if (!payload.vkPayData) {
+						console.error('Отсутствует vkPayData');
+						this.loadWL();
+						return;
+					}
+					localStorage.setItem('termURL', payload.TermUrl);
+					// this.state.TERM_URL = payload.TermUrl;
+					TERM_URL = payload.TermUrl;
+					// this.setState({TERM_URL: payload.TermUrl})
+					window[`gadget_${this.state.gadgetId}`].destroy();
+					console.log('Отправка запроса VK Pay');
+					console.log(payload.vkPayData);
+					connect.send("VKWebAppOpenPayForm", { "app_id": 6777015, "action": "pay-to-service", "params": payload.vkPayData })
+				});
+			};
+			this.loadWL(config);
+		}, 1)
+	}
+
+	componentDidMount() {
+		connect.subscribe((e) => {
+			switch (e.detail.type) {
+				case 'VKWebAppGetUserInfoResult':
+					this.setState({ fetchedUser: e.detail.data });
+					break;
+				case 'VKWebAppAccessTokenReceived':
+					this.setState({ token: e.detail.data.access_token });
+					break;
+				case 'VKWebAppOpenPayFormResult':
+					console.log(e.detail.data);
+					if (e.detail.data.status) {
+						const extraData = JSON.parse(e.detail.data.extra);
+						const PaRes = {
+							paymentToken: {
+								orderId: extraData.order_id,
+								transactionId: e.detail.data.transaction_id
+							}
+						};
+						console.log(TERM_URL);
+						if (TERM_URL == null) {
+							console.error('Отсутствует TERM_URL после удачной оплаты!!!');
+							this.loadWL();
+							return;
+						}
+						console.log('Оплата vk прошла успешно');
+						const fullTermUrl = `${TERM_URL}&PaRes=${encodeURIComponent(JSON.stringify(PaRes))}`;
+						this.startApp(this.state.gadgetPath, this.state.gadgetId, { gadget: { host: fullTermUrl } })
+					} else {
+						console.warn('Оплата vk прошла с ошибкой');
+						console.log(e.detail.data);
+						this.loadWL();
+					}
+					break;
+				case 'VKWebAppOpenPayFormFailed':
+					console.warn('Оплата vk прошла с ошибкой');
+					console.log(e.detail.data);
+					this.loadWL();
+					break;
+				default:
+					console.log(e.detail.type);
+			}
+		});
+		connect.send('VKWebAppGetUserInfo', {});
+		connect.send("VKWebAppSetViewSettings", { "status_bar_style": "light", "action_bar_color": "#212121" });
+	}
+
+	render() {
+		if (this.state.userConnect) {
+
+		}
+		else {
+			return (
+				<div className="panel">
+					<div className="content">
+						<Route exact path="/" render={props =>
+							<Main {...props}
+								statusUser={this.state.statusUser}
+								changeStatusUser={this.changeStatusUser}
+							/>}
+						/>
+						<Route exact path="/railways" render={props =>
+							<Railways {...props}
+								statusUser={this.state.statusUser}
+								changeStatusUser={this.changeStatusUser}
+								startApp={this.startApp}
+							/>}
+						/>
+						<Route exact path="/avia" render={props =>
+							<Avia {...props}
+								statusUser={this.state.statusUser}
+								changeStatusUser={this.changeStatusUser}
+								startApp={this.startApp}
+							/>}
+						/>
+						<Route exact path="/hotel" render={props =>
+							<Hotel {...props}
+								statusUser={this.state.statusUser}
+								changeStatusUser={this.changeStatusUser}
+								startApp={this.startApp}
+							/>}
+						/>
+						<Route exact path="/login" render={props =>
+							<Login {...props}
+								fetchedUser={this.state.fetchedUser}
+								statusUser={this.state.statusUser}
+								changeStatusUser={this.changeStatusUser}
+							/>}
+						/>
+
+					</div>
+				</div>
+			);
+		}
+	}
+}
+
+export default App;
---
+++ front-vk-app/src/components/js/Avia.js
@@ -0,0 +1,102 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+// import { withRouter } from "react-router-dom";
+// import Platform from 'react-platform-js'
+import Icon24Back from '@vkontakte/icons/dist/24/browser_back';
+import connect from '@vkontakte/vkui-connect';
+// import axios from 'axios';
+// import '../css/Avia.css';
+
+import { platform } from '@vkontakte/vkui';
+const osname = platform();
+
+class Avia extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			headerios: "0px",
+			headermain: "58px",
+			maincontent: "calc(100vh - 58px)"
+		}
+	}
+
+	componentDidMount() {
+		if (osname === "android") {
+			console.log("de android")
+			var heightHeader = document.getElementsByClassName('header-main');
+			var heightMainContent = document.getElementsByClassName('main-content');
+			heightHeader[0].style.height = "58px";
+			heightMainContent[0].style.height = "calc(100vh - 58px)";
+		}
+		if (osname === "ios") { console.log("de ios") }
+		if (window.innerWidth >= 936) {
+			var heightMainContent = document.getElementsByClassName('main-content');
+			heightMainContent[0].style.height = "calc(100vh - 130px)";
+		}
+		if (osname === "android") { console.log("de android") }
+		this.props.startApp("https://static.onetwotrip.com/gadgets/vk-app/prod/vk_avia_vk-app/", "5c6bfe146135562ac4c3893c");
+		connect.send("VKWebAppSetViewSettings", { "status_bar_style": "light", "action_bar_color": "#212121" });
+	}
+
+	backApp = () => { this.props.history.go(-1) }
+
+	goApp = (ways) => { this.props.history.push('/' + ways); }
+
+	render() {
+
+		let header; let btloginHead;
+		if (window.innerWidth >= 936) {
+			if (this.props.statusUser) {
+				btloginHead = <div className="header-1-bt-user" onClick={() => { this.goApp('login') }}>
+					<p>Личный кабинет </p>
+				</div>
+			}
+			else {
+				btloginHead = <div className="header-1-bt-user" onClick={() => { this.goApp('login') }} >
+					<p style={{ marginRight: "5px" }}>Войти </p>
+				</div>
+			}
+			header =
+				<div className="header-main" style={{ height: "130px", background: "#3a3a3a" }}>
+					<div className="header-1">
+						<div className="header-2">
+							<div className="header-2-top">
+								<div style={{ width: "72px" }}></div>
+								<div className="header-logo" onClick={() => { this.goApp('') }}><img src="../icon/logo.svg" alt="logo" /></div>
+								{btloginHead}
+							</div>
+							<div className="header-2-bottom" style={{ height: "60px" }}>
+								<div className="header-menu-item" onClick={() => { this.goApp('railways') }}><img src="../icon/train.svg" alt="train" />Ж/д билеты</div>
+								<div className="header-menu-item" onClick={() => { this.goApp('hotel') }}><img src="../icon/hotels.svg" alt="hotels" />Отели</div>
+								<div className="header-menu-item" onClick={() => { this.goApp('avia') }}><img src="../icon/avia.svg" alt="avia" />Авиабилеты</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+		}
+		else {
+			header =
+				<div className="header-main">
+					<div className="header-1">
+						<div className="header-menu-bt" onClick={this.backApp}> <Icon24Back /> </div>
+						<div className="header-logo"><img src="../icon/logo.svg" alt="logo" /></div>
+						<div style={{ width: "28px", height: "28px" }}></div>
+					</div>
+				</div>
+		}
+
+		return (
+			<div className="wrapper">
+				<div className="header-ios" ></div>
+				{header}
+				<div className="main-content" id="main-content" >
+					<div id="whitelabelOTT"></div>
+				</div>
+			</div>
+		);
+
+	}
+}
+
+export default Avia;
